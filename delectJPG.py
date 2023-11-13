021a4a7e82856805dd0c033647ff198b80a5a952
hhyu666 <842761589@qq.com>
2023-11-13 09:01:23 +0800

    10-11

--- delectJPG.py
+++ delectJPG.py
@@ -1,6 +1,6 @@
 import os
 
-folder_path = r"D:\BaiduNetdiskDownload\ai\sd-webui-aki\sd-webui-aki-v4.4\outputs\txt2img-images\10-11"  # 替换为你要删除 PNG 文件所在文件夹的路径
+folder_path = r"D:\BaiduNetdiskDownload\ai\sd-webui-aki\sd-webui-aki-v4.4\outputs\txt2img-images\10-13"  # 替换为你要删除 PNG 文件所在文件夹的路径
 
 def delete_jpg_files(folder_path):
     for root, dirs, files in os.walk(folder_path):
